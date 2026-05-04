755a4e45f294807b530e2c6406f0c1d9734aa4ef
RobotEby <ubuntu.onion@hotmail.com>
2026-05-04 19:19:15 -0300

    feat: add change password DTO

---
+++ src/modules/users/dto/change-password.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, MinLength } from "class-validator";
+
+export class ChangePasswordDto {
+  @ApiProperty()
+  @IsString()
+  current_password!: string;
+
+  @ApiProperty({ minLength: 6 })
+  @IsString()
+  @MinLength(6)
+  new_password!: string;
+}
